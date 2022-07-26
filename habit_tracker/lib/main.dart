a4b45b1db6d4bfbab1e81f7bc3000c81fe6a878e
HelenaKeller <helenakeller92@gmail.com>
2022-07-26 21:46:39 +0200

    renaming title and set primarySwatch to pink

--- habit_tracker/lib/main.dart
+++ habit_tracker/lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Habit tracker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: const StartPage(),
     );
