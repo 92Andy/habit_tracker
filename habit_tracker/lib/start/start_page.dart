b18aa565f8bd9a5768f6346906f01ea6598bfe98
Andy Eckhardt <eckhardtandy@gmail.com>
2022-07-27 22:08:52 +0200

    feature/start-page: added shaker to StartPage

--- habit_tracker/lib/start/start_page.dart
+++ habit_tracker/lib/start/start_page.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Start page ist here!'),
+    return Scaffold(
+      body: Stack(
+        children: const [
+          _StartPageBackground(),
+        ],
+      ),
+    );
+  }
+}
+
+class _StartPageBackground extends StatelessWidget {
+  const _StartPageBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        //Todo: Here should be the color
+        _Shaker(),
+      ],
+    );
+  }
+}
+
+class _Shaker extends StatelessWidget {
+  const _Shaker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: 50.0,
+      child: const SizedBox(
+        height: 650,
+        child: RiveAnimation.asset('assets/rive/shaker.riv'),
       ),
     );
   }
