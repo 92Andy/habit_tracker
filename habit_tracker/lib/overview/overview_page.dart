52fa3d6d8722f815566fc5a033819b326b5a8eed
HelenaKeller <helenakeller92@gmail.com>
2022-07-27 22:14:41 +0200

    feature/overview-page: Overview Page added with bottom navigation bar

---
+++ habit_tracker/lib/overview/overview_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class OverviewPage extends StatelessWidget {
+  const OverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('The Overview Page.'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        backgroundColor: const Color(0xff003dab),
+        selectedItemColor: Colors.amber[800],
+      ),
+    );
+  }
+}
