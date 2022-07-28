fd741ec0b1a1d8d3b433dd9389a052a39bf08003
Andy Eckhardt <eckhardtandy@gmail.com>
2022-07-28 12:11:23 +0200

    feature/start-page: Added background with gradient

--- habit_tracker/lib/start/start_page.dart
+++ habit_tracker/lib/start/start_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+// ignore: implementation_imports
+import 'package:flutter/src/painting/gradient.dart' as gradient;
 import 'package:rive/rive.dart';
 
 class StartPage extends StatelessWidget {
@@ -23,7 +25,7 @@ class _StartPageBackground extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: const [
-        //Todo: Here should be the color
+        _GradientBackground(),
         _Shaker(),
       ],
     );
@@ -40,6 +42,40 @@ class _Shaker extends StatelessWidget {
       child: const SizedBox(
         height: 650,
         child: RiveAnimation.asset('assets/rive/shaker.riv'),
+      ),
+    );
+  }
+}
+
+class _GradientBackground extends StatelessWidget {
+  const _GradientBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: gradient.LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: const [
+            0.1,
+            0.2,
+            0.3,
+            0.4,
+            0.6,
+            0.7,
+            0.8,
+          ],
+          colors: <Color>[
+            const Color(0xffF44FC2).withOpacity(0.2),
+            const Color(0xff8EA7F5).withOpacity(0.25),
+            const Color(0xff8EA7F5).withOpacity(0.2),
+            const Color(0xff8EA7F5).withOpacity(0.5),
+            const Color(0xff8EA7F5).withOpacity(0.4),
+            const Color(0xff8EA7F5).withOpacity(0.7),
+            const Color(0xff8EA7F5),
+          ],
+        ),
       ),
     );
   }
