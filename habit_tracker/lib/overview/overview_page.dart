38df3c9e00c64a6ae17469525b63f0f90a5e4e47
HelenaKeller <helenakeller92@gmail.com>
2022-07-28 15:58:47 +0200

    feature/overview-page: added states for bottom navigation bar

--- habit_tracker/lib/overview/overview_page.dart
+++ habit_tracker/lib/overview/overview_page.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-class OverviewPage extends StatelessWidget {
+class OverviewPage extends StatefulWidget {
   const OverviewPage({Key? key}) : super(key: key);
+
+  @override
+  State<OverviewPage> createState() => _OverviewPage();
+}
+
+class _OverviewPage extends State<OverviewPage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('The Overview Page.'),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -25,7 +54,9 @@ class OverviewPage extends StatelessWidget {
           ),
         ],
         backgroundColor: const Color(0xff003dab),
-        selectedItemColor: Colors.amber[800],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
       ),
     );
   }
