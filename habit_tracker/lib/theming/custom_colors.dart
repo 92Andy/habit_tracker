2e12eaaa810a85b750d6add4fccee97951749f0c
Andy Eckhardt <eckhardtandy@gmail.com>
2022-07-29 15:56:19 +0200

    feature/theming: added custom colors

---
+++ habit_tracker/lib/theming/custom_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static Color primaryPurple = const Color(0xffF44FC2);
+  static Color primaryBlue = const Color(0xff8EA7F5);
+  static Color primaryDarkBlue = const Color(0xff1C03B2);
+}
