0222a051f90cb517994306e13dca17059a5be968
Andy Eckhardt <eckhardtandy@gmail.com>
2022-08-12 18:58:14 +0200

    habit_tracker-feature/add-habit-page: clean code improvements

--- habit_tracker/lib/add_habit/add_habit_page.dart
+++ habit_tracker/lib/add_habit/add_habit_page.dart
@@ -47,54 +47,92 @@ class _AddHabitViewState extends State<_AddHabitView> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Habit title',
-          style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 18),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          height: 60,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Theme.of(context).colorScheme.surface,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Habit title',
+            style:
+                Theme.of(context).textTheme.headline1!.copyWith(fontSize: 18),
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 3,
-              horizontal: 15,
-            ),
-            child: TextField(
-              controller: habitTitleController,
-              textAlignVertical: TextAlignVertical.center,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Enter Habit Title',
-                hintStyle: Theme.of(context).textTheme.bodyText1,
+          const _SmallSpacer(),
+          _WhiteRoundedContainer(
+            height: 60,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 3,
+                horizontal: 15,
+              ),
+              child: TextField(
+                controller: habitTitleController,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Enter Habit Title',
+                  hintStyle: Theme.of(context).textTheme.bodyText1,
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 50),
-        Text(
-          'Choose an Activity',
-          style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 18),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          height: 320,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Theme.of(context).colorScheme.surface,
+          const _MiddleSpacer(),
+          Text(
+            'Choose an Activity',
+            style:
+                Theme.of(context).textTheme.headline1!.copyWith(fontSize: 18),
           ),
-        ),
-        const SizedBox(height: 50),
-        const Center(
-          child: StyledButton(title: 'Add Habit'),
-        ),
-      ],
+          const _SmallSpacer(),
+          _WhiteRoundedContainer(
+            height: 320,
+            child: Container(),
+          ),
+          const _MiddleSpacer(),
+          const Center(
+            child: StyledButton(title: 'Add Habit'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SmallSpacer extends StatelessWidget {
+  const _SmallSpacer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 10);
+  }
+}
+
+class _MiddleSpacer extends StatelessWidget {
+  const _MiddleSpacer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(height: 50);
+  }
+}
+
+class _WhiteRoundedContainer extends StatelessWidget {
+  const _WhiteRoundedContainer({
+    Key? key,
+    required this.height,
+    required this.child,
+  }) : super(key: key);
+
+  final double height;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      child: child,
     );
   }
 }
