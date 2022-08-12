a095fbb7917fee487e7c3d72e6d47f8b37265e8c
Andy Eckhardt <eckhardtandy@gmail.com>
2022-08-12 19:34:52 +0200

    habit_tracker-feature/add-habit-page: "choose activity" container extended (WIP)

--- habit_tracker/lib/add_habit/add_habit_page.dart
+++ habit_tracker/lib/add_habit/add_habit_page.dart
@@ -3,6 +3,33 @@ import 'package:habit_tracker/add_habit/widgets/add_habit_background.dart';
 import 'package:habit_tracker/widgets/custom_shaped_dark_app_bar.dart';
 import 'package:habit_tracker/widgets/styled_button.dart';
 
+final List<Widget> iconButtons = [
+  _CustomIconButton(
+    iconData: Icons.model_training,
+    onTap: () {},
+  ),
+  _CustomIconButton(
+    iconData: Icons.model_training,
+    onTap: () {},
+  ),
+  _CustomIconButton(
+    iconData: Icons.model_training,
+    onTap: () {},
+  ),
+  _CustomIconButton(
+    iconData: Icons.model_training,
+    onTap: () {},
+  ),
+  _CustomIconButton(
+    iconData: Icons.model_training,
+    onTap: () {},
+  ),
+  _CustomIconButton(
+    iconData: Icons.model_training,
+    onTap: () {},
+  ),
+];
+
 class AddHabitPage extends StatelessWidget {
   const AddHabitPage({
     Key? key,
@@ -84,7 +111,43 @@ class _AddHabitViewState extends State<_AddHabitView> {
           const _SmallSpacer(),
           _WhiteRoundedContainer(
             height: 320,
-            child: Container(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25.0),
+              child: Column(
+                children: [
+                  Wrap(
+                    alignment: WrapAlignment.start,
+                    runSpacing: 8,
+                    spacing: 10,
+                    children: iconButtons,
+                  ),
+                  const _MiddleSpacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Select a goal',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Slider(
+                          value: 10,
+                          min: 0,
+                          max: 50,
+                          onChanged: (nextValue) {},
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
           const _MiddleSpacer(),
           const Center(
@@ -128,11 +191,36 @@ class _WhiteRoundedContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: height,
+      width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: Theme.of(context).colorScheme.surface,
       ),
       child: child,
+    );
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  const _CustomIconButton({
+    Key? key,
+    required this.iconData,
+    required this.onTap,
+  }) : super(key: key);
+
+  final IconData iconData;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 30,
+      backgroundColor: Colors.grey.withOpacity(0.2),
+      child: IconButton(
+        icon: Icon(iconData),
+        color: Theme.of(context).colorScheme.onBackground,
+        onPressed: onTap,
+      ),
     );
   }
 }
