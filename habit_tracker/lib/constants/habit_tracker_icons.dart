676961aad42e5caac8134937eb3ced61d965dff1
Andy Eckhardt <eckhardtandy@gmail.com>
2022-08-18 15:03:22 +0200

    habit_tracker-feature/add-habit-page: integrated new icons

---
+++ habit_tracker/lib/constants/habit_tracker_icons.dart
@@ -0,0 +1,12 @@
+///Source: https://www.flaticon.com/free-icons/gym
+///Author: Freepik
+class HabitTrackerIcon {
+  static String get gym => 'assets/icons/gym.png';
+  static String get weightLifting => 'assets/icons/weight-lifting.png';
+  static String get cycling => 'assets/icons/cycling.png';
+  static String get running => 'assets/icons/running.png';
+  static String get skippingRope => 'assets/icons/skipping-rope.png';
+  static String get kettleBell => 'assets/icons/kettlebell.png';
+  static String get swimming => 'assets/icons/swimming.png';
+  static String get more => 'assets/icons/more.png';
+}
