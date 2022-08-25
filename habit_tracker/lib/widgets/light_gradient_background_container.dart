f7aace951aafcd949c8db667fc155108fb551ad8
Andy Eckhardt <eckhardtandy@gmail.com>
2022-08-25 19:58:41 +0200

    feature/overview-page: restructured and renamed (AddHabitBackground)

--- habit_tracker/lib/widgets/light_gradient_background_container.dart
+++ habit_tracker/lib/widgets/light_gradient_background_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/constants/habit_tracker_gradients.dart';
 
-class AddHabitBackground extends StatelessWidget {
-  const AddHabitBackground({Key? key}) : super(key: key);
+class LightGradientBackgroundContainer extends StatelessWidget {
+  const LightGradientBackgroundContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
