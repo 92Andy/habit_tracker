0e1a2704d33c07c46f011b71680ceaa1c4bb578f
Andy Eckhardt <eckhardtandy@gmail.com>
2022-08-26 16:28:51 +0200

    feature/overview-page: adjustments to curvedContainer

--- habit_tracker/lib/overview/overview_page.dart
+++ habit_tracker/lib/overview/overview_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/overview/tabs/main_tab_page.dart';
+import 'package:habit_tracker/constants/habit_tracker_colors.dart';
+import 'package:habit_tracker/constants/habit_tracker_gradients.dart';
 import 'package:habit_tracker/overview/widgets/bottom_nav_bar.dart';
-import 'package:habit_tracker/theming/custom_colors.dart';
-import 'package:habit_tracker/widgets/light_gradient_background_container.dart';
+import 'package:habit_tracker/widgets/curved_container.dart';
 
 class OverviewPage extends StatefulWidget {
   const OverviewPage({Key? key}) : super(key: key);
@@ -14,14 +14,48 @@ class OverviewPage extends StatefulWidget {
 class _OverviewPageState extends State<OverviewPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: const [LightGradientBackgroundContainer()],
+    return Container(
+      decoration: BoxDecoration(
+        gradient: styledLightGradient,
       ),
-      bottomNavigationBar: BottomNavBar(
-        currentIndex: 0,
-        iconTapped: (i) {},
-        navToAddHabit: () {},
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: CurvedContainer(
+                height: 500,
+                boxDecoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      HabitTrackerColors.purple,
+                      HabitTrackerColors.olympicBlue,
+                      HabitTrackerColors.azureBlue,
+                      HabitTrackerColors.olympicBlue,
+                    ],
+                    stops: const [
+                      0.1,
+                      0.2,
+                      0.5,
+                      1,
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: BottomNavBar(
+                currentIndex: 0,
+                iconTapped: (i) {},
+                navToAddHabit: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
