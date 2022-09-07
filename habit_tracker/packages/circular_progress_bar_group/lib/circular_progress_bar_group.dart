63abcdcc82b03eacc1b381613443044ab1e94b1c
Andy Eckhardt <eckhardtandy@gmail.com>
2022-09-07 23:21:43 +0200

    feature/progress-bar: exports defined and test cleaned

--- habit_tracker/packages/circular_progress_bar_group/lib/circular_progress_bar_group.dart
+++ habit_tracker/packages/circular_progress_bar_group/lib/circular_progress_bar_group.dart
@@ -1,7 +1,3 @@
 library circular_progress_bar_group;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'progress_bars/full_circular_progress_bar.dart';
