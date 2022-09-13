a148b39f76a7be2d8c515fe8797c38ed67550c00
Andy Eckhardt <eckhardtandy@gmail.com>
2022-09-13 22:32:54 +0200

    feature/progress-bar: resturcture and renaming

---
+++ habit_tracker/packages/circular_progress_bar_group/lib/progress_bars/triple_splited_progress_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TripleSplitedProgressBar extends StatelessWidget {
+  const TripleSplitedProgressBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
