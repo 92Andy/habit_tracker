b67b3b2045a617ddeb31d4cc3a63a6a3e6aa9bea
Andy Eckhardt <eckhardtandy@gmail.com>
2022-09-13 22:33:20 +0200

    feature/progress-bar: exports adjustments

--- habit_tracker/packages/circular_progress_bar_group/lib/circular_progress_bar_group.dart
+++ habit_tracker/packages/circular_progress_bar_group/lib/circular_progress_bar_group.dart
@@ -1,4 +1,9 @@
 library circular_progress_bar_group;
 
+//bars
 export 'progress_bars/full_circular_progress_bar.dart';
 export 'progress_bars/twice_splited_progress_bar.dart';
+export 'progress_bars/triple_splited_progress_bar.dart';
+
+//models
+export 'progress_bars/models/split_bar_values.dart';
