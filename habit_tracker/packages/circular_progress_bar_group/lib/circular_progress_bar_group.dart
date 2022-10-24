a4932249bc66956f442af872501479e7bb7c9e50
Andy Eckhardt <eckhardtandy@gmail.com>
2022-10-24 17:05:47 +0200

    feature/progress-bar: renaming + restructuring

--- habit_tracker/packages/circular_progress_bar_group/lib/circular_progress_bar_group.dart
+++ habit_tracker/packages/circular_progress_bar_group/lib/circular_progress_bar_group.dart
@@ -1,8 +1,8 @@
 library circular_progress_bar_group;
 
 //bars
-export 'progress_bars/full_circular_progress_bar.dart';
-export 'progress_bars/circular_progress_bar_group.dart';
+export 'circular_progress_bars/full_circular_progress_bar.dart';
+export 'circular_progress_bars/circular_progress_bar_group.dart';
 
 //models
-export 'progress_bars/models/bar_values.dart';
+export 'circular_progress_bars/models/bar_values.dart';
